c3eea1fc6afebde991bde3f55b5ca073da8c26d8
Barry Schneider (MSIH) <bschneider@msih.com>
2022-02-03 07:48:57 -0500

    Working Example

---
+++ BlazorBraintree/wwwroot/braintree.js
@@ -0,0 +1,51 @@
+﻿export function brainstreeDonate(clientToken, dotNetObject, amount, requireAddress = false) {
+    // alert(clientToken);
+    var button = document.querySelector('#submit-button');
+    braintree.dropin.create({
+        authorization: clientToken,
+        selector: '#bt-dropin',
+        applePay: {
+            displayName: 'My Store',
+            paymentRequest: {
+                total: {
+                    label: 'My Store',
+                    amount: amount
+                },
+                if(requireAddress) {
+                    // We recommend collecting billing address information, at minimum
+                    // billing postal code, and passing that billing postal code with all
+                    // Apple Pay transactions as a best practice.
+                    requiredBillingContactFields: ["postalAddress"]
+                }
+            }
+        },
+        paypal: {
+            //flow: 'fault',
+            flow: 'checkout',
+            amount: amount,
+            currency: 'USD'
+        },
+        venmo: {}
+    }, function (err, instance) {
+        if (err) {
+            // An error in the create call is likely due to
+            // incorrect configuration values or network issues.
+            // An appropriate error will be shown in the UI.
+            alert(err);
+            console.error(err);
+            return;
+        }
+        button.addEventListener('click', function () {
+            instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
+                if (requestPaymentMethodErr) {
+                    // No payment method is available.
+                    // An appropriate error will be shown in the UI.
+                    alert(requestPaymentMethodErr);
+                    console.error(requestPaymentMethodErr);
+                    return;
+                }
+                dotNetObject.invokeMethodAsync('ProcessPayment', payload.nonce);
+            });
+        })
+    });
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Blazor-Braintree-Example
+Example using Braintree's global payments platform with ASP.NET Core Blazor
+
+Sandbox site: https://blazorbraintreesandbox.azurewebsites.net/
+
+- Credit Card
+- PayPal
+- Venmo
+- ApplePay
+
+# Setup
